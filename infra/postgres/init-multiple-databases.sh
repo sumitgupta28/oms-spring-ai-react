a3a0615913ec14008539bc06eba648e7e9d3bf23
Sumit Gupta <sumitgupta28@gmail.com>
2026-04-30 23:50:02 -0500

    updated database

--- infra/postgres/init-multiple-databases.sh
+++ infra/postgres/init-multiple-databases.sh
@@ -1,10 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-for db in $(echo "$POSTGRES_MULTIPLE_DATABASES" | tr ',' ' '); do
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        CREATE DATABASE "$db";
-        GRANT ALL PRIVILEGES ON DATABASE "$db" TO "$POSTGRES_USER";
+# The 'oms' database is auto-created by POSTGRES_DB env var.
+# This script creates the keycloak database and per-service schemas inside 'oms'.
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE keycloak;
+    GRANT ALL PRIVILEGES ON DATABASE keycloak TO "$POSTGRES_USER";
 EOSQL
-    echo "Created database: $db"
-done
+echo "Created database: keycloak"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE SCHEMA IF NOT EXISTS products;
+    CREATE SCHEMA IF NOT EXISTS orders;
+    CREATE SCHEMA IF NOT EXISTS payments;
+    GRANT ALL ON SCHEMA products TO "$POSTGRES_USER";
+    GRANT ALL ON SCHEMA orders  TO "$POSTGRES_USER";
+    GRANT ALL ON SCHEMA payments TO "$POSTGRES_USER";
+EOSQL
+echo "Created schemas: products, orders, payments in database $POSTGRES_DB"
